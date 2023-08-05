4a35cfa7fa5ba81310d333adb7734bf72cb79c3b
Nishal Barman <nishalbarman@gmail.com>
2023-08-06 00:02:36 +0530

    Auto parts

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use("", router);
 server.listen(process.env.PORT || 3000, () => {
-  console.log("JSON Server is running");
+  console.log(`JSON Server is running on port ${process.env.PORT || 3000}`);
 });
