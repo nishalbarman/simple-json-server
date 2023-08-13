d126d2bb0b91280215f64d960d97c100458ab519
Nishal Barman <nishalbarman@gmail.com>
2023-08-13 18:48:05 +0530

    changes

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+console.log(jsonServer.defaults());
+const cors = require("cors");
 
 server.use(middlewares);
 server.use("", router);
+server.use(
+  cors({
+    origin: "*",
+  })
+);
 server.listen(process.env.PORT || 3000, () => {
   console.log(`JSON Server is running on port ${process.env.PORT || 3000}`);
 });
